2866703db2ed25c088009dde2d43230d94bbc9e1
debanjandhar12 <debanjandhar12@gmail.com>
2021-11-06 13:11:14 +0530

    chore: ignore typescript errors while importing html templates

--- src/templates/AnkiCardTemplates.ts
+++ src/templates/AnkiCardTemplates.ts
@@ -1,4 +1,6 @@
+// @ts-expect-error
 import frontTemplate from "./front_template.html";
+// @ts-expect-error
 import backTemplate from "./back_template.html";
 export class AnkiCardTemplates {
     static frontTemplate = frontTemplate;
